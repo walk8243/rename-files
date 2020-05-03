1196ad34346df90ce523874766edf62e92089ac7
walk8243 <walk8243@gmail.com>
2020-05-03 23:03:30 +0900

    名前を変更するだけのスクリプトを作成

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log('rename-files');
+import { rename, readdir } from "fs";
+import { resolve, parse } from 'path';
+
+const targetDir = resolve(__dirname, process.argv[2]);
+const numberFormat = new Intl.NumberFormat('ja-JP', { minimumIntegerDigits: 5, useGrouping: false });
+
+readdir(`${targetDir}`, { withFileTypes: true }, (err, files) => {
+	if(err) {
+		console.error(err);
+		return;
+	}
+	const filepaths = files.filter((file) => file.isFile()).map((file) => {
+		const fullpath = resolve(targetDir, file.name);
+		return { ...parse(fullpath), path: fullpath };
+	});
+	filepaths.forEach((filepath, index) => {
+		rename(filepath.path, resolve(filepath.dir, `${numberFormat.format(index)}${filepath.ext}`), (err) => {
+			if(err) {
+				console.error(err);
+				return;
+			}
+		});
+	});
+});
